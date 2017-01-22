7dec7522f6508e3f6ce6d8e485356d2ed72c9ee2
Kyle Carey <krcarey1@asu.edu>
2017-01-22 00:05:39 -0800

    Made login page, and setup hooks for to use username and password

--- app/src/main/java/webdevils/webdevilsapp/MainActivity.java
+++ app/src/main/java/webdevils/webdevilsapp/MainActivity.java
@@ -1,7 +1,11 @@
 package webdevils.webdevilsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button login = (Button) findViewById(R.id.button);
 
+        // Gets username and password and turns them into strings for validation
+        login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText inputName = (EditText) findViewById(R.id.userNameEntry);
+                EditText inputPass = (EditText) findViewById(R.id.passwordEntry);
+                String uName = inputName.getText().toString();
+                String uPass = inputPass.getText().toString();
+            }
+        });
     }
 }
