6566f37fb3d51b75fd2f805c23d62f9ba6a72f5c
Casey Froke <cfroke@asu.edu>
2017-01-31 21:47:31 -0800

    Update ConceptsActivity.java

--- app/src/main/java/webdevils/webdevilsapp/ConceptsActivity.java
+++ app/src/main/java/webdevils/webdevilsapp/ConceptsActivity.java
@@ -4,6 +4,15 @@ package webdevils.webdevilsapp;
  * Created by caseyfroke on 1/29/17.
  */
 
-public class ConceptsActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
+
+public class ConceptsActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_concepts);
+
+    }
 }
