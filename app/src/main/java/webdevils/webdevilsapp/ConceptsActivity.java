084da8c6fd912cfa71433b6adab1ad49cd7e0455
Kyle Carey <krcarey1@asu.edu>
2017-02-06 21:09:26 -0800

    Added hook up for buttons between landing -> my concepts -> Submit concept

--- app/src/main/java/webdevils/webdevilsapp/ConceptsActivity.java
+++ app/src/main/java/webdevils/webdevilsapp/ConceptsActivity.java
@@ -4,8 +4,11 @@ package webdevils.webdevilsapp;
  * Created by caseyfroke on 1/29/17.
  */
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class ConceptsActivity extends AppCompatActivity{
@@ -13,6 +16,15 @@ public class ConceptsActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_concepts);
+
+        Button submitNewConcept = (Button) findViewById(R.id.button2);
+
+        submitNewConcept.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), SubmitConceptActivity.class);
+                startActivity(i);
+            }
+        });
 
     }
 }
