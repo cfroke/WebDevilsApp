ea56fbf885d20688da8137c10a0b7ee47cdd06de
Martin Gasiorowski <mjgasior@asu.edu>
2017-03-03 16:02:27 -0700

    Pushing for visibility

--- app/src/main/java/webdevils/webdevilsapp/OwnedConcept.java
+++ app/src/main/java/webdevils/webdevilsapp/OwnedConcept.java
@@ -1,13 +1,13 @@
 package webdevils.webdevilsapp;
 
-import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,8 +19,7 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import common.Concept;
 import server.Services;
 
-public class ConceptDetailsActivity extends Fragment {
-    private Concept thisConcept;
+public class OwnedConcept extends Fragment {
 
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
@@ -28,13 +27,42 @@ public class ConceptDetailsActivity extends Fragment {
      */
     private GoogleApiClient client;
 
+    private Concept thisConcept;
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        Button btnSave = (Button) getView().findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //setDescription() added to concept class, need server solution.
+            }
+        });
+
+        Button btnDelete = (Button) getView().findViewById(R.id.btnDelete);
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Need method of deleting concept. Should run confirmation dialog first.
+            }
+        });
+
+        TextView txtTitle = (TextView) getView().findViewById(R.id.txtTitle);
+        txtTitle.setText(thisConcept.getTitle() + "(" + thisConcept.getUpvoteStatus() + ")");
+        TextView txtBody = (TextView) getView().findViewById(R.id.txtBody);
+        txtTitle.setText(thisConcept.getDescription());
+    }
+
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
     public Action getIndexApiAction() {
         Thing object = new Thing.Builder()
-                .setName("ConceptDetails Page") // TODO: Define a title for the content shown.
+                .setName("OwnedConcept Page") // TODO: Define a title for the content shown.
                 // TODO: Make sure this auto-generated URL is correct.
                 .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
                 .build();
@@ -42,10 +70,6 @@ public class ConceptDetailsActivity extends Fragment {
                 .setObject(object)
                 .setActionStatus(Action.STATUS_TYPE_COMPLETED)
                 .build();
-    }
-
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.activity_concept_details, container, false);
     }
 
     @Override
@@ -56,26 +80,6 @@ public class ConceptDetailsActivity extends Fragment {
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client.connect();
         AppIndex.AppIndexApi.start(client, getIndexApiAction());
-
-        Button btnDownVote = (Button) getView().findViewById(R.id.btnDownVote);
-        btnDownVote.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Services.downVoteConcept(thisConcept);
-            }
-        });
-
-        Button btnUpVote = (Button) getView().findViewById(R.id.btnUpVote);
-        btnUpVote.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Services.upVoteConcept(thisConcept);
-            }
-        });
-
-        TextView txtTitle = (TextView) getView().findViewById(R.id.txtTitle);
-        txtTitle.setText(thisConcept.getTitle());
-        TextView txtBody = (TextView) getView().findViewById(R.id.txtBody);
-        TextView txtScore = (TextView) getView().findViewById(R.id.txtVoteScore);
-        txtScore.setText("Score: " + thisConcept.getUpvoteStatus());
     }
 
     @Override
